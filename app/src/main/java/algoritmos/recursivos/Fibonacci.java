6a4049422de081524f1aedec75e028d7397071bb
luiz.eduardo40 <luiz.eduardo40@hotmail.com>
2025-05-22 20:28:04 -0300

    Implementando algoritmo iterativo para cálculo da sequência de fibonacci

--- app/src/main/java/algoritmos/recursivos/Fibonacci.java
+++ app/src/main/java/algoritmos/recursivos/Fibonacci.java
@@ -11,6 +11,23 @@ public class Fibonacci {
         }
     }
 
+    public static int fibonacciIterativo(int n) {
+        int i, fibonacci = 0;
+        int fibAnterior = 0;
+        int fibPosterior = 0;
+        if ((n == 0) || (n == 1)) {
+            return 1;
+        }
+        else {
+            for (i = 2; i <= n; i++) {
+                fibonacci = fibAnterior + fibPosterior;
+                fibAnterior = fibPosterior;
+                fibPosterior = fibonacci;
+            }
+            return fibonacci;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 6;
         int resultado = fibonacciRecursivo(n);
